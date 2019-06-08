73086513f83ee472b622d2f04e9e8d3b06868847
Ozan Erdal <oerdal@uw.edu>
2019-06-07 23:25:40 -0700

    pushing extra plots and tests

--- scrape_test.py
+++ scrape_test.py
@@ -1,10 +1,9 @@
-from cse163_utils import assert_equals
+from scraper_utils import assert_equals
 
 import project_demo as demo
 import scraper_utils
 
 import pandas as pd
-import numpy as np
 
 CURRENT_YEAR_DATA = './shots/current_year_shot_data.csv'
 ALL_YEAR_DATA = './shots/all_year_shot_data.csv'
@@ -16,12 +15,18 @@ all_data = pd.read_csv(ALL_YEAR_DATA)
 def test_project_demo():
     print('Testing project demo')
 
-    # Checks that each year is storing 5 players' information
-    assert_equals(5, len(set(curr_data['PLAYER_NAME'])))
+    # Checks that each year is storing 250 players' information
+    assert_equals(250, len(set(curr_data['PLAYER_NAME'])))
 
     # Checks that data was collected for 19 seasons (2000-2019)
+    # and that the clean_shots function works
+    print('Cleaning shots')
+    cleaned_shots = demo.clean_shots(all_data)
     dates = all_data.sort_values(by='GAME_DATE')['GAME_DATE']
+
     assert_equals(19, dates[len(all_data) - 1]//10000 - dates[0]//10000)
+    assert_equals(19, cleaned_shots.at[len(cleaned_shots) - 1, 'YEAR'] -
+                  cleaned_shots.at[0, 'YEAR'])
 
 
 def test_scraper_utils():
